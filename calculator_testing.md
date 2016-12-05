a36c611dc92f43dec384cfed29dc09ad14b21af9
Dirk Dunn <dirkdunn91@gmail.com>
2016-12-05 16:30:12 -0600

    first push

---
+++ calculator_testing.md
@@ -0,0 +1,16 @@
+# Calculator testing  
+
+We are going to put our testing skills to the test(pun intended) by writing some tests on an existing piece of code.
+
+Within **testing_homework.js**, we have a `calculator` object, along with some starter testing code, so far we have one test already, which tests the `calculator.add()` function, let's finish the rest of the tests!
+
+
+### Directions
+1. Write an it() test for every method within `calculator`
+2. If there are any bugs( there are :) ) fix them on the calculator object.
+3. Make all of your tests that you write are correct.
+4. Make all of your tests pass.
+
+**BONUS:**  
+Fill out the `calculator.random` function to return a random integer between 1-10(including 10).
+Write a test for this to test that it's always returning a number between 1-10.
---
+++ testing_homework.js
@@ -0,0 +1,32 @@
+var expect = require('chai').expect;
+var calculator = {
+  /* Basic arithmatic functions */
+  add: function(a,b){ return a + b; },
+  subtract: function(a,b){ return a - b; },
+  multiply: function(a,b){ return a * a; },
+  divide: function(a,b){ return a / b; },
+  squared: function(n){ return n * n; },
+  /* E.G. power(2,3) is two to the third power */
+  power: function(n,p){ return Math.pow(n,p); },
+  /* E.G. squareRoot(9) is 3 */
+  squareRoot: function(n){ return Math.sqrt(n); },
+  /* Look up the Math object on W3 schools or Mozilla */
+  /* E.G. Rounds up a number roundUp(3.1) will be "4" */
+  roundUp: function(n){ return Math.floor(n); },
+  /* E.G. Rounds down a number roundUp(3.1) will be "3" */
+  roundDown: function(n){ return Math.ceil(n); },
+  /* BONUS: write a function that returns a random number between 1-10,
+  and figure out a way to test it, *Use stackOverflow* HINT: a loop when testing */
+  random: function(){
+  }
+};
+
+/* Write Mocha/Chai Tests here */
+/* Starter Code */
+describe('Calculator functions work', function(){
+
+  it('calculator.add works correctly',function(){
+    expect(calculator.add(1,2)).to.equal(3);
+  });
+
+});
