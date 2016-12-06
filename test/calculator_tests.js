fadc607709b47781925efc1d5f5de3d35443199b
Stephanie Workman <sworkman2014@gmail.com>
2016-12-06 08:53:30 -0600

    wrote in tests for calculator

--- test/calculator_tests.js
+++ test/calculator_tests.js
@@ -17,16 +17,52 @@ var calculator = {
   roundDown: function(n){ return Math.ceil(n); },
   /* BONUS: write a function that returns a random number between 1-10,
   and figure out a way to test it, *Use stackOverflow* HINT: a loop when testing */
-  random: function(){
+  random: function(n){ return Math.floor(Math.random()*10); },
   }
 };
-
 /* Write Mocha/Chai Tests here */
 /* Starter Code */
 describe('Calculator functions work', function(){
 
   it('calculator.add works correctly',function(){
     expect(calculator.add(1,2)).to.equal(3);
+  });
+
+  it('calculator.subtract works correctly', function(){
+    expect(calculator.subtract(2,1)).to.equal(3);
+  });
+
+  it('calculator.multiply works correctly', function(){
+    expect(calculator.multiply(2,2)).to.equal(4);
+  });
+
+  it('calculator.divide works correcrly', function(){
+    expect(calculator.divide(25,5)).to.equal(5);
+  });
+
+  it('calculator.squared works correctly', function(){
+    expect(calculator.squard(5,5)).to.equal(25);
+  });
+
+  it('calculator.power works correctly', function(){
+    expect(calculator.power(6,2)).to.equal(36);
+  });
+
+  it('calculator.squareRoot works correctly', function(){
+    expect(calculator.squareRoot(16)).to.equal(4);
+  });
+
+  it('calculator.roundUp works correctly', function(){
+    expect(calculator.roundUp(4.2)).to.equal(5);
+  });
+
+  it('calculator.roundDown works correctly', function(){
+    expect(calculator.roundDown(4.2)).to.equal(4);
+  });
+
+  it('calculator.random works correctly', function(){
+    expect(calculator.random(3.4)).to.equal(3);
+
   });
 
 });
